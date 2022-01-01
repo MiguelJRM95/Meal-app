2c39261c122eee984412d51eeb513b95d1859373
MiguelJRM95 <miguelitoworkout@gmail.com>
2022-01-01 19:04:28 +0100

    Navigator created

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import * as Font from 'expo-font';
+
+const fetchFonts = async () => {
+  return await Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  
+  if(!fontLoaded){
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => setFontLoaded(true)} 
+      />
+    );
+  }
+
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
